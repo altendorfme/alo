0aa9d4bdc6616dd6c209af962aee4b79ed60012f
Renan Bernordi <altendorfme@gmail.com>
2025-02-27 23:09:55 -0300

    [alert]: fixing

--- app/templates/layout/default.php
+++ app/templates/layout/default.php
@@ -125,31 +125,48 @@
             <?php
             if (isset($error)) {
                 ?>
-                <div class="alert alert-danger"><?= $error; ?></div>
+                <div class="alert alert-danger">
+                    <span class="bi bi-exclamation-triangle-fill me-2"></span>
+                    <?= $error; ?>
+                </div>
                 <?php
             } elseif (isset($_GET['error'])) {
                 ?>
-                <div class="alert alert-danger"><?= _e($_GET['error']); ?></div>
+                <div class="alert alert-danger">
+                    <?= _e($_GET['error']); ?>
+                </div>
                 <?php
             }
 
             if (isset($warning)) {
                 ?>
-                <div class="alert alert-alert"><?= $warning; ?></div>
+                <div class="alert alert-warning">
+                    <span class="bi bi-exclamation-triangle-fill me-2"></span>
+                    <?= $warning; ?>
+                </div>
                 <?php
             } elseif (isset($_GET['warning'])) {
                 ?>
-                <div class="alert alert-alert"><?= _e($_GET['warning']); ?></div>
+                <div class="alert alert-warning">
+                    <span class="bi bi-exclamation-triangle-fill me-2"></span>
+                    <?= _e($_GET['warning']); ?>
+                </div>
                 <?php
             }
 
             if (isset($success)) {
                 ?>
-                <div class="alert alert-success"><?= $success; ?></div>
+                <div class="alert alert-success">
+                    <span class="bi bi-check-circle-fill me-2"></span>
+                    <?= $success; ?>
+                </div>
                 <?php
             } elseif (isset($_GET['success'])) {
                 ?>
-                <div class="alert alert-success"><?= _e($_GET['success']); ?></div>
+                <div class="alert alert-success">
+                    <span class="bi bi-check-circle-fill me-2"></span>
+                    <?= _e($_GET['success']); ?>
+                </div>
                 <?php
             }
             ?>
--- app/templates/layout/login.php
+++ app/templates/layout/login.php
@@ -16,37 +16,54 @@
                         <i class="bi bi-bell-fill d-inline-block fs-1"></i>
                     </div>
 
-                    <?php
-                    if (isset($error)) {
-                        ?>
-                        <div class="alert alert-danger"><?= $error; ?></div>
-                        <?php
-                    } elseif (isset($_GET['error'])) {
-                        ?>
-                        <div class="alert alert-danger"><?= _e($_GET['error']); ?></div>
-                        <?php
-                    }
+					<?php
+					if (isset($error)) {
+						?>
+						<div class="alert alert-danger">
+							<span class="bi bi-exclamation-triangle-fill me-2"></span>
+							<?= $error; ?>
+						</div>
+						<?php
+					} elseif (isset($_GET['error'])) {
+						?>
+						<div class="alert alert-danger">
+							<?= _e($_GET['error']); ?>
+						</div>
+						<?php
+					}
 
-                    if (isset($warning)) {
-                        ?>
-                        <div class="alert alert-alert"><?= $warning; ?></div>
-                        <?php
-                    } elseif (isset($_GET['warning'])) {
-                        ?>
-                        <div class="alert alert-alert"><?= _e($_GET['warning']); ?></div>
-                        <?php
-                    }
+					if (isset($warning)) {
+						?>
+						<div class="alert alert-warning">
+							<span class="bi bi-exclamation-triangle-fill me-2"></span>
+							<?= $warning; ?>
+						</div>
+						<?php
+					} elseif (isset($_GET['warning'])) {
+						?>
+						<div class="alert alert-warning">
+							<span class="bi bi-exclamation-triangle-fill me-2"></span>
+							<?= _e($_GET['warning']); ?>
+						</div>
+						<?php
+					}
 
-                    if (isset($success)) {
-                        ?>
-                        <div class="alert alert-success"><?= $success; ?></div>
-                        <?php
-                    } elseif (isset($_GET['success'])) {
-                        ?>
-                        <div class="alert alert-success"><?= _e($_GET['success']); ?></div>
-                        <?php
-                    }
-                    ?>
+					if (isset($success)) {
+						?>
+						<div class="alert alert-success">
+							<span class="bi bi-check-circle-fill me-2"></span>
+							<?= $success; ?>
+						</div>
+						<?php
+					} elseif (isset($_GET['success'])) {
+						?>
+						<div class="alert alert-success">
+							<span class="bi bi-check-circle-fill me-2"></span>
+							<?= _e($_GET['success']); ?>
+						</div>
+						<?php
+					}
+					?>
 
                     <?= $this->section('page_content') ?>
                 </div>
