59443d51cff38e68e41c345f4db4efa7c7dd31a9
Renan Bernordi <altendorfme@gmail.com>
2025-06-18 14:23:00 -0300

    [migration]: fix max key length

--- app/migration/4-2025-06-18.sql
+++ app/migration/4-2025-06-18.sql
@@ -1,4 +1,4 @@
-ALTER TABLE segment_goals 
-ADD INDEX IF NOT EXISTS idx_segment_value_optimized (segment_id, value);
+ALTER TABLE segment_goals
+ADD INDEX IF NOT EXISTS idx_segment_value_optimized (segment_id, value(191));
 
 ANALYZE TABLE segments, segment_goals;
